2a642eff84cb01167f7cca5dedaee0bd197fae4e
sungmin <24ci0133@jec.ac.jp>
2025-11-10 16:43:01 +0900

    Fade out Dotween作った

---
+++ MusicGame/Assets/Script/Title/Ooo_Fade.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using DG.Tweening;
+ 
+
+public class Ooo_Fade : MonoBehaviour
+{
+    [SerializeField] public Image fadeImage;  //フェード画像
+    [SerializeField] public TextMeshProUGUI fadeText;    //フェードテキスト
+    [SerializeField] private float fadeDuration;   //フェード時間
+
+    void Update()
+    {
+        fadeImage.DOFade(0f, fadeDuration);
+        fadeText.DOFade(0f, fadeDuration);
+    }
+}
