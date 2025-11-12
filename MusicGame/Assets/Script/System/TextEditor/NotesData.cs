a1beea2cb17d2da29e7cfb1a5795a2dc5e2dbab0
Morita <22ci0244@jec.ac.jp>
2025-11-12 16:46:31 +0900

    キーコードを変更途中　エラー出るのでプル非推奨

--- MusicGame/Assets/Script/System/TextEditor/NotesData.cs
+++ MusicGame/Assets/Script/System/TextEditor/NotesData.cs
@@ -4,12 +4,25 @@ using System.Collections.Generic;
 using System.IO;
 
 namespace Notes {
+    public enum Direction
+    {
+        Non = -1, Top, Right, Down, Left
+    }
 
     // ノーツを召喚するための構造体
     public struct Notes
     {
-
-
+        public Notes(float time_, int dirN, bool isH, int lane_)
+        {
+            time = time_;
+            dir = (Direction)dirN;
+            isHold = isH;
+            lane = lane_;
+        }
+        float time;
+        Direction dir;
+        bool isHold;
+        int lane;
     }
 
     // 曲に対応したノーツのデータを保存するクラス
