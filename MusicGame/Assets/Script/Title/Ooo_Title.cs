5e6c2a9f416379061235827b90437e8390770499
sungmin <24ci0133@jec.ac.jp>
2025-11-12 15:20:16 +0900

    title

--- MusicGame/Assets/Script/Title/Ooo_Title.cs
+++ MusicGame/Assets/Script/Title/Ooo_Title.cs
@@ -1,10 +1,11 @@
 ﻿using UnityEngine;
 using TMPro;
-using UnityEngine.SceneManagement;
+using DG.Tweening;
+using UnityEngine.UI;
 
 public class Ooo_Title : MonoBehaviour
 {
-    
+    [SerializeField] Image TitleImage;
 
     void Start()
     {
@@ -14,6 +15,9 @@ public class Ooo_Title : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       
+        //イメージ回転
+        transform.DORotate(new Vector3(0f, 0f, 360f), 3f, RotateMode.FastBeyond360);    //5秒でｚ軸を360度回転
+        //イメージスケイル
+        transform.DOScale(new Vector3(5f, 5f, 0f), 1f);     //1秒後X、Y方向を2倍に変更
     }
 }
