42aa36d43330e2ed8800eddfe07b55cefe855ffe
Bisu1noshin <24ci0211@jec.ac.jp>
2025-11-18 11:31:30 +0900

    ノーツ終わり

--- MusicGame/Assets/Script/Notes/Generator/NotesGenerator.cs
+++ MusicGame/Assets/Script/Notes/Generator/NotesGenerator.cs
@@ -4,6 +4,38 @@ namespace Notes {
 
     public class NotesGenerator : MonoBehaviour
     {
+        private NotesData notesData;
+        private float timeCnt;
 
+        // ノーツのプレファブ
+        private GameObject Flick;
+        private GameObject Hold;
+        private GameObject Rush;
+
+        private void Awake()
+        {
+            // ノーツの読み込み
+            Flick = Resources.Load<GameObject>("");
+            Hold = Resources.Load<GameObject>("");
+            Rush = Resources.Load<GameObject>("");
+        }
+
+        private void Start()
+        {
+            
+        }
+
+        private void Update()
+        {
+            
+        }
+
+        private void CreateNotes(float time,NotesParent notes) {
+
+            if (time >= timeCnt) {
+
+
+            }
+        }
     }
 }
