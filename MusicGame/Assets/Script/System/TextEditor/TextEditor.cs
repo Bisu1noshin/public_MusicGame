f089d4583a61f86cd2b965d5330bcbce89c9056d
kameda <mizukikamedajp@gmail.com>
2025-11-19 16:42:46 +0900

    データロードの準備　クラスとかも色々いじったので確認よろ

--- MusicGame/Assets/Script/System/TextEditor/TextEditor.cs
+++ MusicGame/Assets/Script/System/TextEditor/TextEditor.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using Notes;
+using NUnit.Framework;
+using System;
 using UnityEngine;
 
 namespace TextEditor {
 
     public class TextEditor<DataClass>
-        where DataClass : class
+        where DataClass : class, INotesListEditor
     {
         // メンバー変数
         static string NotesRootPath = default; //譜面全体を管理するフォルダのパス
@@ -19,12 +21,59 @@ namespace TextEditor {
         /// テキストからデータを取得変換
         /// </summary>
         /// <param name="data"></param>
-        public void GetData(DataClass data) {
+        public void GetData(DataClass data1, DataClass data2) {
 
             // 処理に失敗したらnull
-            if (data == null) return;
+            if (data1 == null || data2 == null) return;
 
-
+            TextAsset ta = Resources.Load(filePath) as TextAsset;
+            string[] strs = ta.text.Split('\n');
+            int textCnt = 0;
+            foreach (string str in strs)
+            {
+                if (textCnt == 1)
+                {
+                    int bpm_ = int.Parse(str);
+                }
+                if (textCnt > 1)
+                {
+                    string[] notesInfo = str.Split(",");
+                    int infoCnt = 0;
+                    int time = 0, dirN = 0, typeN = 0, laneN = 0;
+                    foreach (string note in notesInfo)
+                    {
+                        int value = int.Parse(note);
+                        switch (infoCnt)
+                        {
+                            case 0:
+                                time = value;
+                                break;
+                            case 1:
+                                dirN = value;
+                                break;
+                            case 2:
+                                typeN = value;
+                                break;
+                            case 3:
+                                laneN = value;
+                                break;
+                            default:
+                                break;
+                        }
+                        infoCnt++;
+                    }
+                    Notes.Notes notes = new(time, dirN, typeN);
+                    if (laneN == 0)
+                    {
+                        data1.AddNotes(notes);
+                    }
+                    else
+                    {
+                        data2.AddNotes(notes);
+                    }
+                }
+                textCnt++;
+            }
         }
         public string GetFilePath(string musicName, string level)
         {
