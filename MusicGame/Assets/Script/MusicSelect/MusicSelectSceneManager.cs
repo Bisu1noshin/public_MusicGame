cd3017fbb545d4a1eff89a31938f4efb482a3886
kameda <mizukikamedajp@gmail.com>
2025-11-25 18:12:55 +0900

    楽曲選択シーン作成中

---
+++ MusicGame/Assets/Script/MusicSelect/MusicSelectSceneManager.cs
@@ -0,0 +1,55 @@
+﻿using NUnit.Framework;
+using Player;
+using UnityEngine;
+
+public class MusicSelectSceneManager : MonoBehaviour, IMusicSelecter
+{
+    Music.MusicData musicData;
+    int selectNum;
+    Player.Player_forMusicSelect player;
+
+    private void Awake()
+    {
+
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+        selectNum = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void LoadMusics()
+    {
+
+    }
+    public void GoForward()
+    {
+
+    }
+    public void GoBack()
+    {
+
+    }
+    public void Enter()
+    {
+        
+    }
+    public void Undo()
+    {
+
+    }
+}
+
+public interface IMusicSelecter
+{
+    void GoForward();
+    void GoBack();
+    void Enter();
+    void Undo();
+}
---
+++ MusicGame/Assets/Script/Player/Player_forMusicSelect.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Player
+{
+    public enum ButtonKind
+    {
+        A, B, X, Y
+    }
+
+    public class Player_forMusicSelect : PlayerParent
+    {
+        IMusicSelecter musicSelecter;
+        float LStick_onTime, RStick_onTime;
+        private void Start()
+        {
+            musicSelecter = GameObject.Find("SceneManager").GetComponent<MusicSelectSceneManager>();
+            LStick_onTime = 0;
+        }
+
+        private void Update()
+        {
+
+        }
+        protected override void OnButtonA()
+        {
+            musicSelecter.Enter();
+        }
+        protected override void OnButtonB()
+        {
+            musicSelecter.Undo();
+        }
+        protected override void OnButtonX() { }
+        protected override void OnButtonY() { }
+        protected override void UpButtonA() { }
+        protected override void UpButtonB() { }
+        protected override void UpButtonX() { }
+        protected override void UpButtonY() { }
+
+        protected override void LookUpdate(Vector2 vec)
+        {
+            
+        }
+        protected override void MoveUpdate(Vector2 vec)
+        {
+            if (vec == Vector2.zero)
+            {
+                LStick_onTime = 0;
+                return;
+            }
+
+
+            LStick_onTime += Time.deltaTime;
+        }
+    }
+}
