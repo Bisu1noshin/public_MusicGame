f6fb01150b1f3f4c3005356d3121a0b520dfc579
Bisu1noshin <24ci0211@jec.ac.jp>
2025-11-25 12:07:16 +0900

    ノーツは終わり

--- MusicGame/Assets/Script/Notes/Generator/NotesDataConversion.cs
+++ MusicGame/Assets/Script/Notes/Generator/NotesDataConversion.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.IO;
 
 namespace Notes
 {
@@ -9,11 +12,52 @@ namespace Notes
         public NotesDataConversion(NotesData data)
         {
             this.data = data;
+            DataConversion();
         }
 
         private void DataConversion()
         {
+            for (int i = 0; i < data.notes.Length; i++)
+            {
+                int holdCnt = 0;
+                NotesKind preKind = data.notes[i][0].kind;
 
+                for (int j = 0; j < data.notes[i].Count;j++)
+                {
+                    if (data.notes[i][j].kind != preKind)
+                    {
+                        if (holdCnt > 0)
+                        {
+                            Notes pre = data.notes[i][j - 1];
+
+                            data.notes[i][j - 1] =
+                                new Notes(
+                                    pre.time,
+                                    (int)pre.dir,
+                                    (int)pre.kind,
+                                    holdCnt
+                                );
+
+                            holdCnt = 0;
+                        }
+
+                        preKind = data.notes[i][j].kind;
+                        continue;
+                    }
+
+                    if (data.notes[i][j].kind != NotesKind.Flick)
+                    {
+                        data.notes[i].RemoveAt(j);
+                        j--;
+                        holdCnt++;
+                    }                
+                }
+            }     
+        }
+
+        public NotesData GetData()
+        {
+            return data;
         }
     }
 }
