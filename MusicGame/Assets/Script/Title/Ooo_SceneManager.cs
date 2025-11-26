d71cc8e8e882592b1114b24bbeb067c13a125163
sungmin <24ci0133@jec.ac.jp>
2025-11-26 14:57:54 +0900

    scene manager追加

--- MusicGame/Assets/Script/Title/Ooo_SceneManager.cs
+++ MusicGame/Assets/Script/Title/Ooo_SceneManager.cs
@@ -6,6 +6,6 @@ public class Ooo_SceneManager : MonoBehaviour
    public void SceneChange(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
-        Debug.Log("Credit Scene");
+        Debug.Log(sceneName + "へ移動");
     }
 }
