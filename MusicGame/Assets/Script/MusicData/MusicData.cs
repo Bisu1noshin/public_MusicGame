dfe6e6fa2b5214b8b587371f44bae97f427c9c60
jecci5 <24ci0204@jec.ac.jp>
2025-12-01 16:05:11 +0900

    曲データをScriptable Objectで管理します

--- MusicGame/Assets/Script/MusicData/MusicData.cs
+++ MusicGame/Assets/Script/MusicData/MusicData.cs
@@ -1,7 +1,18 @@
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "MusicData", menuName = "Scriptable Objects/MusicData")]
-public class MusicData : ScriptableObject
+public class MusicDatabase : ScriptableObject
 {
-    
+    public List<MusicData> musicDatabase;
+}
+
+[System.Serializable]
+public class MusicData
+{
+    public int musicNo;
+    public string musicName;
+    public string composerName;
+    public string musicPath; //"Resource/name.mp3"
+    public int BPM;
 }
