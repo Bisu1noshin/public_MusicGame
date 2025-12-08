0c87c701bdeb596d17159ceca32696fd8d562eeb
kameda <mizukikamedajp@gmail.com>
2025-12-08 19:24:00 +0900

    細部を変更

--- MusicGame/Assets/Script/MusicSelect/LevelButtonController.cs
+++ MusicGame/Assets/Script/MusicSelect/LevelButtonController.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class LevelButtonController : MonoBehaviour
 {
+    public static float posX = 0.0f;
     public ButtonState mState { get; private set; }
     ILevelSelecter mSelecter;
     TextMeshProUGUI mText;
     RectTransform mRect;
     int id;
-    float timer;
 
     void Awake()
     {
@@ -23,21 +23,63 @@ public class LevelButtonController : MonoBehaviour
             Debug.LogError("Error! : SceneManger is NOT found.");
         }
         mRect = GetComponent<RectTransform>();
+        mState = ButtonState.Appear;
     }
-
     // Update is called once per frame
     void Update()
     {
-        if (mSelecter.SelectNum[1] == id)
+        switch (mState)
         {
-            transform.localScale = Vector3.one * 1.2f;
-        }
-        else
-        {
-            transform.localScale = Vector3.one;
-        }
+            case ButtonState.Appear:
+                float x = Mathf.Lerp(mRect.anchoredPosition.x, posX, 0.2f);
+                mRect.anchoredPosition = new(x, mRect.anchoredPosition.y);
+                if (Mathf.Abs(x - posX) < 1.0f)
+                {
+                    mRect.anchoredPosition = new(posX, mRect.anchoredPosition.y);
+                    mState = ButtonState.Active;
+                }
+                break;
 
-        mRect.anchoredPosition = new Vector2(-350.0f, (id + 1) * -200.0f + 510.0f);
+            case ButtonState.Active:
+
+                if (mSelecter.SelectNum[1] == id)
+                {
+                    transform.localScale = Vector3.one * 1.2f;
+                }
+                else
+                {
+                    transform.localScale = Vector3.one;
+                }
+                mRect.anchoredPosition = new Vector2(posX, SetY(id));
+                break;
+            case ButtonState.Dead:
+                mRect.localScale = Vector3.one;
+                switch (mSelecter.mSceneState)
+                {
+                    case SceneState.MusicSelect:
+                        x = Mathf.Lerp(mRect.anchoredPosition.x, 1360.0f, 0.2f);
+                        mRect.anchoredPosition = new(x, mRect.anchoredPosition.y);
+                        if (Mathf.Abs(1360.0f - x) < 1.0f)
+                        {
+                            Destroy(gameObject);
+                        }
+                        break;
+                    case SceneState.LevelSelect:
+                        Destroy(gameObject);
+                        break;
+                    case SceneState.EnterGame:
+                        x = Mathf.Lerp(mRect.anchoredPosition.x, -1360.0f, 0.2f);
+                        mRect.anchoredPosition = new(x, mRect.anchoredPosition.y);
+                        if (Mathf.Abs(-1360.0f - x) < 0.1f)
+                        {
+                            Destroy(gameObject);
+                        }
+                        break;
+                }
+                
+                break;
+        }
+        
     }
     public void SetProperty(int id_, string str_)
     {
@@ -45,19 +87,32 @@ public class LevelButtonController : MonoBehaviour
         mText.text = str_;
         mState = ButtonState.Appear;
     }
-    public static Action CreateInstance(int id_, string str_)
+    public static float SetY(int id_)
+    {
+        return (id_ + 1) * -200.0f + 350.0f;
+    }
+    public static Action CreateInstance(int id_, string str_, SceneState ss = SceneState.LevelSelect)
     {
         GameObject res = Resources.Load("MusicSelecter/LevelButton") as GameObject;
         GameObject go = Instantiate(res);
         go.transform.SetParent(GameObject.Find("Canvas").transform.GetChild(1).transform);
-        go.transform.SetLocalPositionAndRotation(new(-350, 0, 0), Quaternion.identity);
+        go.transform.localRotation = Quaternion.identity;
         go.name = str_;
         go.transform.localScale = Vector3.one;
         
         LevelButtonController lbc = go.GetComponent<LevelButtonController>();
         lbc.SetProperty(id_, str_);
+        if (ss == SceneState.EnterGame)
+        {
+            lbc.mRect.anchoredPosition = new(-1360.0f, SetY(id_));
+        }
+        else
+        {
+            lbc.mRect.anchoredPosition = new(1360.0f, SetY(id_));
+        }
+        
 
-        Action action = () => { Destroy(go); };
+        Action action = () => { lbc.mState = ButtonState.Dead; };
         return action;
     }
 }
