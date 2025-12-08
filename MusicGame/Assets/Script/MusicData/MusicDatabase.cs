e66db081dc953c3a833c9f169238a12a86f099f3
jecci5 <24ci0204@jec.ac.jp>
2025-12-08 16:33:36 +0900

    ジャケット、デモ音源の追加・データベースの更新

--- MusicGame/Assets/Script/MusicData/MusicDatabase.cs
+++ MusicGame/Assets/Script/MusicData/MusicDatabase.cs
@@ -13,9 +13,10 @@ public class MusicData
     public int id;
     public string name;
     public string composerName; //作曲者名
-    public string musicPath; //"Resource/name.mp3"
+    public string musicPath; //"folder/name.mp3"
     public string demoMusicPath;
     public int BPM;
+    public string jacketPath;
     public string normalPath;
     public string hardPath;
     public string expertPath;
