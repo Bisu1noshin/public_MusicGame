737c7872282f6bc1655f31bf9c34d5e8aa6ab305
Bisu1noshin <24ci0211@jec.ac.jp>
2025-12-30 02:55:43 +0900

    インターネット関連のアセット追加に伴う実験

---
+++ MusicGame/Assets/Script/OnLine/Player/InGamePlayer.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using Mirror;
+
+namespace OnLine
+{
+    public class InGamePlayer : NetworkBehaviour
+    {
+        // 定期更新時に呼ばれる
+        void FixedUpdate()
+        {
+            // ローカルプレイヤーの時
+            if (isLocalPlayer)
+            {
+                // 操作
+                float x = Input.GetAxis("Horizontal");
+                float z = Input.GetAxis("Vertical");
+                CmdMoveSphere(x, z);
+            }
+        }
+
+        // 球の移動
+        [Command]
+        void CmdMoveSphere(float x, float z)
+        {
+            Vector3 v = new Vector3(x, 0, z) * 5f;
+            GetComponent<Rigidbody>().AddForce(v);
+        }
+    }
+
+}
