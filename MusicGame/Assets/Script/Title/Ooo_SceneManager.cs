7c6246e656ea7fd516d80667bbbf3f9b97f0114e
kameda <mizukikamedajp@gmail.com>
2026-01-14 18:05:11 +0900

    オちゃんのタイトルシーンにLoadSceneRefを追加　校閲求む

--- MusicGame/Assets/Script/Title/Ooo_SceneManager.cs
+++ MusicGame/Assets/Script/Title/Ooo_SceneManager.cs
@@ -1,8 +1,10 @@
-﻿using UnityEngine;
+﻿using LoadForAsync;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Ooo_SceneManager : MonoBehaviour
 {
+    [SerializeField] AssetLoadConfig mAssetResources;
     private void Awake()
     {
         Application.targetFrameRate = 60;
@@ -14,7 +16,8 @@ public class Ooo_SceneManager : MonoBehaviour
         if (Input.anyKeyDown)
         {
             // シーン遷移
-            SceneChange("Test_ModeSelectScene");
+            //SceneChange("Test_ModeSelectScene");
+            LoadSceneRef();
         }
     }
 
@@ -22,5 +25,28 @@ public class Ooo_SceneManager : MonoBehaviour
     {
         SceneManager.LoadScene(sceneName);
         Debug.Log(sceneName + "へ移動");
+    }
+
+    private async void LoadSceneRef(/*string musicPath_, string notesPath_*/)
+    {
+        // 選択された楽曲とノーツのファイルを設定
+        //foreach (var obj in mAssetResources.ReferencesAssets)
+        //{
+        //    if (obj.ObjectPath == "Music")
+        //    {
+        //        // リストの中から musicPath_ と一致するものを探す
+        //        var found = musicList.Find(x => x.key == musicPath_);
+        //        if (found != null) obj.AssetReference = found.assetRef;
+        //    }
+
+        //    if (obj.ObjectPath == "TextAsset")
+        //    {
+        //        var found = textList.Find(x => x.key == notesPath_);
+        //        if (found != null) obj.AssetReference = found.assetRef;
+        //    }
+        //}
+
+        string naxtSceneName = "Test_ModeSelectScene";
+        await DataTransferSystem.LoadSceneRef(mAssetResources, naxtSceneName);
     }
 }
