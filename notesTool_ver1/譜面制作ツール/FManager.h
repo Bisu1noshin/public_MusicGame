98bf05a803707c56bf70631d5124540332099b44
jecci5 <24ci0204@jec.ac.jp>
2026-01-28 14:48:16 +0900

    NewYorkBackRaiseHARD前半完成

--- notesTool_ver1/譜面制作ツール/FManager.h
+++ notesTool_ver1/譜面制作ツール/FManager.h
@@ -45,8 +45,8 @@ private:
 	float time;
 
 	float bpm; //4分音符が1分間に入る個数
-	std::string musicName = "./data/NewYorkBackRaise_2nd.mp3"; //使う音楽のファイル名を入れる
-	const std::string filename = "./data/NewYorkBackRaise_EXPERT_2nd.txt";
+	std::string musicName = "./data/NewYorkBackRaise.mp3"; //使う音楽のファイル名を入れる
+	const std::string filename = "./data/NewYorkBackRaise_HARD.txt";
 
 	NotesManager* mNotesManager;
 	std::stringstream ss;
